557f810d4323036441cb64d723c6fa9c8173d5c2
Akash <akashvit1090@gmail.com>
2022-04-15 10:18:28 +0530

    Added About Page

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const http = require("http");
 
 http
     .createServer((req,res)=>{
-        console.log(res);
-        res.write("<h1>Home page!</h1>");
-        res.end();
+        switch (http.request.url) {
+            case "/":
+                response.writeHead(200,{"Content-Type":"test/html",
+            });
+            response.write("<h1>Home page!</h1>");
+            response.end();
+            break;
+            case"/about":
+                respnse.writeHead(404,{
+                    "Content-Type":"text/html",
+                });
+                response.write("<p>About Page!</p>");
+                response.end();
+                break;
+        }
     })
     .listen(8080);
